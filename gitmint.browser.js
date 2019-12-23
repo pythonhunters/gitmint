c3f766095a004f1b7b51dd34f30467ad5b3e0c7f
laike9m <laike9m@gmail.com>
2019-12-22 19:19:20 -0800

    try

--- gitmint.browser.js
+++ gitmint.browser.js
@@ -4246,8 +4246,11 @@ var Gitmint =
         const urlObject = new URL(url);
 
         // https://developer.github.com/v3/issues/comments/#reactions-summary
-        if (urlObject.pathname.endsWith('comments') || urlObject.pathname.endsWith('issues') || urlObject.pathname.endsWith('reactions')) {
-          req.setRequestHeader('Accept', 'application/vnd.github.squirrel-girl-preview+json,application/vnd.github.html+json');
+        if (urlObject.pathname.endsWith('comments') || urlObject.pathname.endsWith('issues')) {
+          req.setRequestHeader('Accept', 'application/vnd.github.html+json,application/vnd.github.squirrel-girl-preview+json');
+        }
+        if (urlObject.pathname.endsWith('reactions')) {
+          req.setRequestHeader('Accept', 'application/vnd.github.squirrel-girl-preview+json');
         }
         req.setRequestHeader('Authorization', 'token ' + token);
         if (method !== 'GET' && method !== 'DELETE') {
